1540831dafc6927ae95865ab056260c5ec13cac5
zulsstore2806-ship-it <zulsstore2806@gmail.com>
2026-09-02 00:06:19 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#f5f3ff',
+          100: '#ede9fe',
+          500: '#8b5cf6',
+          600: '#7c3aed',
+          700: '#6d28d9',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
